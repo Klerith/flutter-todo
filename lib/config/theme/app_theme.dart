a9ed17ced4f5e9325fa1f0515b1e344f815e48aa
Fernando Herrera <fernando.herrera85@gmail.com>
2024-01-06 12:18:19 -0500

    Update text theme in AppTheme

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -6,6 +6,14 @@ class AppTheme {
   static ThemeData get lightTheme {
     return ThemeData.light().copyWith(
       scaffoldBackgroundColor: const Color(0xffE7E9ED),
+
+      textTheme: const TextTheme(
+        titleLarge: TextStyle(
+          fontSize: 23,
+          fontWeight: FontWeight.w400,
+          color: Colors.black,
+        ),
+      )
     );
   }
 
